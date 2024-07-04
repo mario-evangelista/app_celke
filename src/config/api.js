a8b24a1b1470d59eac2cffe3b2086cc9838dc21c
Mário Evangelista <mariojbe@gmail.com>
2024-07-04 14:12:06 -0300

    Curso criar API com Laravel 11 e aplicativo com React Native

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Substituir o IP 192.168.15.200 pelo o IP da máquina que está a api. Para encontrar o IP: ipconfig
-  baseURL: "http://172.30.254.52:8000/api/",
+  baseURL: "http://127.0.0.1:8000/api/",
 });
 
 export default api;
